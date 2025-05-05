147c741c8213db60a150cc1c9aeb8696b114ebae
Jayesh Rathi <itsjayeshrathi@gmail.com>
2025-05-05 14:55:55 +0530

    added the archive functionality in walk cli

--- walk-cli/main_test.go
+++ walk-cli/main_test.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
+)
+
+var (
+	buffer    bytes.Buffer
+	logBuffer bytes.Buffer
 )
 
 func TestRun(t *testing.T) {
@@ -51,6 +61,180 @@ func TestRun(t *testing.T) {
 			if tc.expected != res {
 				t.Errorf("Expected %q, got %q instead\n", tc.expected, res)
 			}
+		})
+	}
+}
+
+func TestRunDelExtension(t *testing.T) {
+	testCases := []struct {
+		name        string
+		cfg         config
+		extNoDelete string
+		nDelete     int
+		nNoDelete   int
+		expected    string
+	}{
+		{
+			name:        "DeleteExtensionNoMatch",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: ".gz",
+			nDelete:     0,
+			nNoDelete:   10,
+			expected:    "",
+		},
+		{
+			name:        "DeleteExtensionMatch",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: "",
+			nDelete:     10,
+			nNoDelete:   0,
+			expected:    "",
+		},
+		{
+			name:        "DeleteExtensionMixed",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: ".gz",
+			nDelete:     5,
+			nNoDelete:   5,
+			expected:    "",
+		},
+	}
+	for _, tc := range testCases {
+
+		t.Run(tc.name, func(t *testing.T) {
+			logBuffer.Reset()
+			tc.cfg.wLog = &logBuffer
+			var buffer bytes.Buffer
+
+			tempDir := createTempDir(t, map[string]int{
+				tc.cfg.ext:     tc.nDelete,
+				tc.extNoDelete: tc.nNoDelete,
+			})
+
+			if err := run(tempDir, &buffer, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			res := buffer.String()
+
+			if tc.expected != res {
+				t.Errorf("Expected %q, got %q instead\n", tc.expected, res)
+			}
+
+			filesLeft, err := os.ReadDir(tempDir)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(filesLeft) != tc.nNoDelete {
+				t.Errorf("Expected %d files left, got %d instead\n", tc.nNoDelete, len(filesLeft))
+			}
+
+			expLogLines := tc.nDelete + 1
+
+			lines := bytes.Split(logBuffer.Bytes(), []byte("\n"))
+
+			if len(lines) != expLogLines {
+				t.Errorf("Expected %d log lines, got %d instead\n",
+					expLogLines, len(lines))
+			}
+
+		})
+	}
+}
+
+func createTempDir(t *testing.T, files map[string]int) (dirname string) {
+	t.Helper()
+	tempDir, err := os.MkdirTemp("", "walktree")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Register cleanup to remove the temporary directory
+	t.Cleanup(func() {
+		os.RemoveAll(tempDir)
+	})
+
+	for k, n := range files {
+
+		for j := 1; j <= n; j++ {
+
+			fname := fmt.Sprintf("file%d%s", j, k)
+			fpath := filepath.Join(tempDir, fname)
+			if err := os.WriteFile(fpath, []byte("dummy"), 0644); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	return tempDir
+}
+
+func RunTestArchive(t *testing.T) {
+	testCases := []struct {
+		name         string
+		cfg          config
+		extNoArchive string
+		nArchive     int
+		nNoArchive   int
+	}{
+		{
+			name:         "ArhiveExtensionNoMatch",
+			cfg:          config{ext: ".log"},
+			extNoArchive: ".gz",
+			nArchive:     0,
+			nNoArchive:   10,
+		},
+		{
+			name:         "ArchiveExtensionMatch",
+			cfg:          config{ext: ".log"},
+			extNoArchive: "",
+			nArchive:     10,
+			nNoArchive:   0,
+		},
+		{
+			name:         "ArchiveExtensionMixed",
+			cfg:          config{ext: ".log"},
+			extNoArchive: ".gz",
+			nArchive:     5,
+			nNoArchive:   5,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var buffer bytes.Buffer
+
+			// Create temp dirs for RunArchive test
+			tempDir := createTempDir(t, map[string]int{
+				tc.cfg.ext:      tc.nArchive,
+				tc.extNoArchive: tc.nNoArchive,
+			})
+
+			archiveDir := createTempDir(t, nil)
+			tc.cfg.archive = archiveDir
+			if err := run(tempDir, &buffer, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+			pattern := filepath.Join(tempDir, fmt.Sprintf("*%s", tc.cfg.ext))
+			expFiles, err := filepath.Glob(pattern)
+			if err != nil {
+				t.Fatal(err)
+			}
+			expOut := strings.Join(expFiles, "\n")
+			res := strings.TrimSpace(buffer.String())
+			if expOut != res {
+				t.Errorf("Expected %q, got %q instead\n", expOut, res)
+			}
+			filesArchived, err := ioutil.ReadDir(archiveDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(filesArchived) != tc.nArchive {
+				t.Errorf("Expected %d files archived, got %d instead\n",
+					tc.nArchive, len(filesArchived))
+			}
+
 		})
 	}
 }
--- walk-cli/mian.go
+++ walk-cli/mian.go
@@ -4,17 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 type config struct {
-	ext  string
-	size int64
-	list bool
+	ext     string
+	size    int64
+	list    bool
+	del     bool
+	archive string
+	wLog    io.Writer
 }
 
+var (
+	f   = os.Stdout
+	err error
+)
+
 func run(root string, out io.Writer, cfg config) error {
+	delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
 
 	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -26,6 +36,14 @@ func run(root string, out io.Writer, cfg config) error {
 		if cfg.list {
 			return listFile(path, out)
 		}
+		if cfg.archive != "" {
+			if err := archiveFile(cfg.archive, root, path); err != nil {
+				return err
+			}
+		}
+		if cfg.del {
+			return delFile(path, delLogger)
+		}
 		return listFile(path, out)
 	})
 }
@@ -33,16 +51,30 @@ func run(root string, out io.Writer, cfg config) error {
 func main() {
 
 	root := flag.String("root", ".", "Root directory to start.")
+	logFile := flag.String("log", "", "Log deletes to this file")
 	ext := flag.String("ext", "", "Extension to filter out.")
 	size := flag.Int64("size", 0, "Minimum File size to filter out.")
 	list := flag.Bool("list", false, "List files only.")
-
+	del := flag.Bool("del", false, "Delete files only.")
+	archive := flag.String("archive", "", "Archive file.")
 	flag.Parse()
 
 	c := config{
-		ext:  *ext,
-		size: *size,
-		list: *list,
+		ext:     *ext,
+		size:    *size,
+		list:    *list,
+		del:     *del,
+		archive: *archive,
+		wLog:    f,
+	}
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
